7f04464d0546fdddfdf0034c52c3e4e25b091aa6
H. Nikolaus Schaller <hns@goldelico.com>
2025-06-28 18:53:00 +0200

    retrode-web-control: mapper: add Cart Doctor, Select Mapper, Raw Read sections
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- retrode3-debian-packages/retrode-web-control/var/www/html/mapper.php
+++ retrode3-debian-packages/retrode-web-control/var/www/html/mapper.php
@@ -3,14 +3,21 @@
 include("header.inc.php");
 
 ?>
-<h2>Select Mapper</h2>
-Here could be other fancy things. Perhaps on separate pages...
-<input name="mapper" type="text" width="20">
+<h3>Cart Doctor</h3>
+Add tools to detect card contents
+<h3>Select Mapper</h3>
+Manually select mapper: <input name="mapper" type="text" width="20">
 <p>
 <input type="checkbox">Checkbox</input>
 <p>
 <input type="radio">Radio</input>
 <p>
+
+<h3>Raw Read</h3>
+Do a raw read.
+
+<h3>Cart Flasher</h3>
+Flash Carts with EEPROM.
 
 <?php
 include("footer.inc.php");
