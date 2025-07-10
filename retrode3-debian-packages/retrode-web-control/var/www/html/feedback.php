c38c95a84395059afd385d524244771cd6a28fa2
H. Nikolaus Schaller <hns@goldelico.com>
2025-07-10 17:29:57 +0200

    retrode-web-control: add documentation to feedback page
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- retrode3-debian-packages/retrode-web-control/var/www/html/feedback.php
+++ retrode3-debian-packages/retrode-web-control/var/www/html/feedback.php
@@ -3,12 +3,19 @@
 include("header.inc.php");
 
 ?>
+<h2>Preliminary Documentation</h2>
+<p>
+<a href="documentation.html">Documentation</a> ... should be in an external Wiki!
+</p>
 <h2>Feedback</h2>
+<p>
 We are happy to teake your feedback and consider for improvements!
-
+</p>
+<p>
 Please send an e-mail: <a href="mailto:feedback@retrode.com">Feedback</a>.
+<p>
 Or look up <a href="https://www.retrode.com">Info Pages</a>.
-
+</p>
 <?php
 include("footer.inc.php");
 ?>
