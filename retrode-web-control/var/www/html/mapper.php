8d64f23a86552ea1750afcdd6a2f5ecf7fd47b88
H. Nikolaus Schaller <hns@goldelico.com>
2026-04-25 19:42:58 +0200

    retrode-web-control: mapper.php: allow to dump MegaDrive RAM
    Signed-off-by: H. Nikolaus Schaller <hns@goldelico.com>

--- retrode-web-control/var/www/html/mapper.php
+++ retrode-web-control/var/www/html/mapper.php
@@ -3,17 +3,9 @@
 include("header.inc.php");
 
 ?>
-<h3>Select Mapper</h3>
-Manually select mapper: <input name="mapper" type="text" width="20">
+<h3>Raw Access</h3>
 <p>
-<input type="checkbox">Checkbox</input>
-<p>
-<input type="radio">Radio</input>
-<p>
-
-<h3>Raw Read</h3>
-<p>
-<a href="?dump=headers">Dump Headers</a>
+<a href="?dump=headers">Dump ROM Headers</a>
 </p>
 <?php
 $dump=getvar("dump");
@@ -27,14 +19,37 @@ switch($dump)
 	}
 ?>
 
+<h3>RAM Tools</h3>
+<p>
+<a href="?dump=ram">Dump RAM</a>
+</p>
+<?php
+$dump=getvar("dump");
+switch($dump)
+	{
+	case "ram":
+		html("<pre>");
+		text(callcmd("sudo /usr/local/bin/retrode-dump $dump"));
+		html("</pre>");
+		break;
+	}
+?>
+
+<h2>Experimental or not implemented</h2>
 <h3>Cart Doctor</h3>
 Add tools to detect card contents. Like calling ucon64.
 
-<h3>RAM Tools</h3>
-Add tools to read/write RAM content.
-
 <h3>Cart Flasher</h3>
 Flash Carts with EEPROM.
+
+<h3>Select Mapper</h3>
+Manually select mapper: <input name="mapper" type="text" width="20">
+<p>
+<input type="checkbox">Checkbox</input>
+<p>
+<input type="radio">Radio</input>
+<p>
+
 
 <?php
 include("footer.inc.php");
